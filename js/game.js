b289f5c63f1ed6b31d0a731a81c02b09f0030c11
Oliver Lau <oliver@ersatzworld.net>
2025-02-27 17:49:23 +0100

    Restructured some code.

--- js/game.js
+++ js/game.js
@@ -281,16 +281,7 @@
                 directionLookup[`${dir.dx},${dir.dy}`] = key;
             });
             Object.freeze(directionLookup);
-            const ProbTableTurns = {
-                N: 0,
-                NE: 1,
-                E: 2,
-                SE: 3,
-                S: 4,
-                SW: 5,
-                W: 6,
-                NW: 7,
-            };
+            const ProbTableTurns = { N: 0, NE: 1, E: 2, SE: 3, S: 4, SW: 5, W: 6, NW: 7, };
             Object.freeze(ProbTableTurns);
             let path;
             let current = { x: undefined, y: undefined };
